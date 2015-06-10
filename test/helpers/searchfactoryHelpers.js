3899ad2be98df25c4f49a77a275a63d392ea3036
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-10 17:59:28 +0100

    last two commits were me being silly... added basic style to page

---
+++ test/helpers/searchfactoryHelpers.js
@@ -0,0 +1,32 @@
+function searchFactoryHelperMethod() {
+
+	var search;
+	var httpBackend;
+	var items = [
+		{
+			"login": "tansaku",
+			"avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+			"html_url": "https://github.com/tansaku"
+		},
+		{
+			"login": "stephenlloyd",
+			"avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+			"html_url": "https://github.com/stephenlloyd"
+		}
+	];
+
+	beforeEach(module('GitUserSearch'));
+
+	beforeEach(inject(function(Search) {
+		search = Search;
+	}));
+
+	beforeEach(inject(function($httpBackend) {
+		httpBackend = $httpBackend;
+		httpBackend
+			.expectGET("https://api.github.com/search/users?q=jellypants")
+			.respond(
+				{ items: items }
+			);
+	}));
+};
