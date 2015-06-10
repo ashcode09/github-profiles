af99c450a9344fbe3ae2279e2e9c0723b575c87b
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-10 16:56:53 +0100

    passed unit test for search factory - returns search results and has proper functionality

--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -8,7 +8,38 @@ describe('factory: Search', function() {
 		search = Search;
 	}));
 
+	var httpBackend;
+
+	beforeEach(inject(function($httpBackend) {
+		httpBackend = $httpBackend;
+		httpBackend
+			.expectGET("https://api.github.com/search/users?q=jellypants")
+			.respond(
+				{ items: items }
+			);
+	}));
+
+	var items = [
+		{
+			"login": "tansaku",
+			"avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+			"html_url": "https://github.com/tansaku"
+		},
+		{
+			"login": "stephenlloyd",
+			"avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+			"html_url": "https://github.com/stephenlloyd"
+		}
+	];
+
 	it('responds to query', function() {
 		expect(search.query).toBeDefined();
+	});
+
+	it('returns search results', function() {
+		search.query('jellypants')
+		.then(function(response) {
+			expect(response.data).toEqual(items);
+		});
 	});
 });
