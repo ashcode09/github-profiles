87ccd6aef17bdeda40dd00b5dc80e3c65658d6be
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-11 16:25:48 +0100

    refactored feature tests and still passing

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,14 +1,19 @@
 describe('Github Profile Finder', function() {
 
-	it('has a title function', function() {
+	var inputBox = element(by.model('searchCtrl.searchTerm'));
+	var searchButton = element(by.className('btn'))
+
+	beforeEach(function() {
 		browser.get('http://localhost:8080');
+	});
+
+	it('has a title function', function() {
 		expect(browser.getTitle()).toEqual('GitHub User Search');
 	});
 
 	it('finds profiles', function() {
-		browser.get('http://localhost:8080');
-		element(by.model('searchCtrl.searchTerm')).sendKeys('ashleigh090990');
-		element(by.className('btn')).click();
+		inputBox.sendKeys('ashleigh090990');
+		searchButton.click();
 		expect(element(by.binding('user.login')).getText()).toEqual('ashleigh090990');
 	});
 
