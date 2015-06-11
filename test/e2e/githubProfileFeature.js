98118864f8a1c9edac78ee80a80895f4c5268b65
Ashleigh Maund <ashleighmaund@gmail.com>
2015-06-11 16:16:57 +0100

    passed feature test - github profile finder can find profiles

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -6,3 +6,14 @@ describe('Github Profile Finder', function() {
 	});
 
 });
+
+describe('Github profile finder', function() {
+
+	it('finds profiles', function() {
+		browser.get('http://localhost:8080');
+		element(by.model('searchCtrl.searchTerm')).sendKeys('ashleigh090990');
+		element(by.className('btn')).click();
+		expect(element(by.binding('user.login')).getText()).toEqual('ashleigh090990');
+	});
+
+});
